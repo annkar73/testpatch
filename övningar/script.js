b6a9220b7facd3f4673d069cf16b5cac1077ec3b
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2023-11-09 09:32:52 +0100

    added code

--- övningar/script.js
+++ övningar/script.js
@@ -1,1 +1,21 @@
-const myName= 'Anna';
+let myName = 'Anna';
+console.log('Hej' + ' ' + myName.toUpperCase());
+
+const petAge = 12;
+const myAge = 50;
+const jointBirthdayParty = petAge + myAge;
+console.log('Kom på kalas, nu fyller vi' + ' ' + jointBirthdayParty);
+
+const timeOfDay = 'morgon';
+console.log('Jag hoppas ni har en god' + ' ' + timeOfDay);
+
+const btnAccept = document.querySelector('#btnAccept');
+console.log(btnAccept);
+btnAccept.textContent = 'Challenge Accepted!';
+btnAccept.style.backgroundColor = 'green';
+btnAccept.style.color = 'white';
+btnAccept.style.border = 'none';
+btnAccept.style.padding = '8px 4px';
+
+let myCity = 'Chicago';
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
